cbbf4a4d6260fa7cdbbc3fd55b1b3d62d84c46ee
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2021-06-11 22:00:12 +0530

    added new production api url

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -1,43 +1,42 @@
-import React, { useState } from "react";
-import { Link, Route, useHistory } from "react-router-dom";
+import React, {useState} from "react";
+import {Link, useHistory} from "react-router-dom";
 import "../CSS/Signin.css";
-/**import { FaUser } from "react-icons/fa";**/
 import Signup from "./Signup";
 import boyVector from "../Images/boy_vector_signin.svg";
 import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:8000/api/",
+  baseURL: `https://api-book-recommender.herokuapp.com/api/`
 });
 
 const Signin = () => {
   const signUp = (e) => {
     e.preventDefault();
     <Link to="/signup">
-      <Signup />
+      <Signup/>
     </Link>;
   };
 
   return (
-    <div className="container">
-      <SigninForm />
-      <div className="panel-container">
-        <div className="content">
-          <h3>New Here?</h3>
-          <p>Create an account with us and get benefits of our system</p>
-          <button
-            className="light_btn"
-            onClick={() => {
-              signUp();
-            }}
-          >
-            Sign Up
-          </button>
+      <div className="container">
+        <SigninForm/>
+        <div className="panel-container">
+          <div className="content">
+            <h3>New Here?</h3>
+            <p>Create an account with us and get benefits of our system</p>
+            <button
+                className="light_btn"
+                onClick={() => {
+                  signUp();
+                }}
+            >
+              Sign Up
+            </button>
+          </div>
+          <img src={boyVector} alt="Boy" className="image"/>
         </div>
-        <img src={boyVector} alt="Boy" className="image" />
       </div>
-    </div>
   );
 };
 const SigninForm = () => {
@@ -66,53 +65,53 @@ const SigninForm = () => {
       passwordInvalid.innerHTML = output;
     } else {
       instance
-        .post("login", {
-          email: email,
-          password: password,
-        })
-        .then(function (response) {
-          // console.log(response);
+          .post("login", {
+            email: email,
+            password: password,
+          })
+          .then(function (response) {
+            // console.log(response);
 
-          if (response.status === 200) {
-            history.replace("/");
-          }
-        })
-        .catch((error) => {
-          // alert(error);
+            if (response.status === 200) {
+              history.replace("/");
+            }
+          })
+          .catch((error) => {
+            // alert(error);
 
-          const output = `<small>${error}</small>`;
-          passwordInvalid.innerHTML = output;
-        });
+            const output = `<small>${error}</small>`;
+            passwordInvalid.innerHTML = output;
+          });
     }
   };
   return (
-    <div className="forms-container">
-      <div className="signin-signup">
-        <form action="#" class="sign-in-form">
-          <h2 className="title">Sign in</h2>
-          <div className="input-field">
-            <input
-              type="text"
-              placeholder="Email"
-              onChange={(e) => handleInput1(e)}
-            />
-          </div>
+      <div className="forms-container">
+        <div className="signin-signup">
+          <form action="#" class="sign-in-form">
+            <h2 className="title">Sign in</h2>
+            <div className="input-field">
+              <input
+                  type="text"
+                  placeholder="Email"
+                  onChange={(e) => handleInput1(e)}
+              />
+            </div>
 
-          <div className="input-field">
-            <input
-              type="password"
-              placeholder="Password"
-              onChange={(e) => handleInput2(e)}
-            />
-          </div>
-          <div className="password__invalid"></div>
+            <div className="input-field">
+              <input
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e) => handleInput2(e)}
+              />
+            </div>
+            <div className="password__invalid"></div>
 
-          <button type="submit" className="dark_btn" onClick={(e) => signIn(e)}>
-            Sign In
-          </button>
-        </form>
+            <button type="submit" className="dark_btn" onClick={(e) => signIn(e)}>
+              Sign In
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
   );
 };
 export default Signin;
