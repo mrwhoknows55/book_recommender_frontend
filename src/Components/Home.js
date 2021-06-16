1f24efa1a97c364378de856079c728ebc9c00dc4
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2021-06-16 18:11:26 +0530

    if not logged in then will redirected to signin

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import React, { useState } from "react";
-import { useHistory } from "react-router";
+import React, {useState} from "react";
+import {useHistory} from "react-router";
 import "../CSS/Home.css";
 import instance from "../Utils/axios";
 
@@ -13,40 +13,43 @@ const Home = () => {
     const instance = axios.create({
       withCredentials: true,
       baseURL: `https://api-book-recommender.herokuapp.com/api/`,
-      headers: { Authentication: token },
+      headers: {Authentication: token},
     });
     instance.get("user").then((resp) => setName(resp.data.name));
+  } else {
+    window.sessionStorage.removeItem("token");
+    history.replace("/signin");
   }
   const logOut = (e) => {
     e.preventDefault();
 
     if (token) {
       instance
-        .post("logout", {
-          headers: { Authentication: token },
-        })
-        .then((response) => {
-          // TODO
-          // if (response.data.success) {
-          // }
-        });
+          .post("logout", {
+            headers: {Authentication: token},
+          })
+          .then((response) => {
+            // TODO
+            // if (response.data.success) {
+            // }
+          });
       window.sessionStorage.removeItem("token");
       history.replace("/signin");
     }
   };
 
   return (
-    <div className="home">
-      <h1>Hello {name ? name : "Guest"}</h1>
-      <button
-        className="logout__btn"
-        onClick={(e) => {
-          logOut(e);
-        }}
-      >
-        LogOut
-      </button>
-    </div>
+      <div className="home">
+        <h1>Hello {name ? name : "Guest"}</h1>
+        <button
+            className="logout__btn"
+            onClick={(e) => {
+              logOut(e);
+            }}
+        >
+          LogOut
+        </button>
+      </div>
   );
 };
 
