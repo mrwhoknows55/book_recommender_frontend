a4b015b637032b01408462680b832ea120b688fa
unknown <shivamraut07@gmail.com>
2021-06-18 23:42:04 +0530

    Animation Done with errors at backend

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -1,45 +1,33 @@
-import React, {useState} from "react";
+import React from "react";
+import "../CSS/Signin.css";
+import boyVector from "../Images/boy.svg";
+import womenVector from "../Images/women.svg";
 import EmailIcon from "@material-ui/icons/Email";
 import LockIcon from "@material-ui/icons/Lock";
-import {useHistory} from "react-router-dom";
-import "../CSS/Signin.css";
-import boyVector from "../Images/boy_vector_signin.svg";
+import PersonIcon from "@material-ui/icons/Person";
 import instance from "../Utils/axios";
 import emailValidation from "../Utils/emailValidation";
 import swal from 'sweetalert';
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
-const Signin = () => {
+
+function Signin() {
   const history = useHistory();
-
-  const signUp = (e) => {
-    e.preventDefault();
-    history.push("/signup");
+  const signUpAnimate = (e) => {
+    const container = document.querySelector("#animate");
+    if (container) {
+      container.classList.add("sign-up-mode");
+    }
   };
 
-  return (
-      <div className="container">
-        <SigninForm/>
-        <div className="panel-container">
-          <div className="content">
-            <h3>New Here?</h3>
-            <p>Create an account with us and get benefits of our system</p>
-            <button
-                className="light_btn"
-                onClick={(e) => {
-                  signUp(e);
-                }}
-            >
-              Sign Up
-            </button>
-          </div>
-          <img src={boyVector} alt="Boy" className="image"/>
-        </div>
-      </div>
-  );
-};
-const SigninForm = () => {
-  const history = useHistory();
-  const [email, setEmail] = useState("");
+  const signInAnimate = (e) => {
+    const container = document.querySelector("#animate");
+    if (container) {
+      container.classList.remove("sign-up-mode");
+    }
+  };
+const [email, setEmail] = useState("");
   const [password, setPassword] = useState();
 
   const handleInput1 = (e) => {
@@ -58,62 +46,218 @@ const SigninForm = () => {
     console.log(password);
     const empty__input = document.querySelector(".empty__input");
     if (
-        email !== "" &&
-        email !== undefined &&
-        emailValidation(email) &&
-        password !== "" &&
-        password !== undefined
+      email !== "" &&
+      email !== undefined &&
+      emailValidation(email) &&
+      password !== "" &&
+      password !== undefined
     ) {
       instance
-          .post("login", {
-            email: email,
-            password: password,
-          })
-          .then(function (response) {
-            if (response.status === 200) {
-              window.sessionStorage.setItem("token", response.data.token);
-              history.replace("/");
-            }
-          })
-          .catch((error) => {
-            swal("Something Went Wrong", error.response.data.detail, "error");
-          });
+        .post("login", {
+          email: email,
+          password: password,
+        })
+        .then(function (response) {
+          if (response.status === 200) {
+            window.sessionStorage.setItem("token", response.data.token);
+            history.replace("/");
+          }
+        })
+        .catch((error) => {
+          const output = `<small>${error}</small>`;
+          empty__input.innerHTML = output;
+        });
     } else {
-      swal("Error", "Please enter valid details. ", "error");
+      swal("Alert", "Please enter valid details. ", "error");
     }
   };
+   const signUp = (e) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password1, setPassword1] = useState();
+  const [password2, setPassword2] = useState();
+  const empty__input = document.querySelector(".empty__input");
+  const handleInput1 = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+    empty__input.innerHTML = "";
+  };
+  const handleInput2 = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+    empty__input.innerHTML = "";
+  };
+  const handleInput3 = (e) => {
+    e.preventDefault();
+    setPassword1(e.target.value);
+    empty__input.innerHTML = "";
+  };
+  const handleInput4 = (e) => {
+    e.preventDefault();
+    setPassword2(e.target.value);
+    empty__input.innerHTML = "";
+  };
+
+  const signUp = (e) => {
+    e.preventDefault();
+    console.log(name);
+    console.log(email);
+    console.log(password1);
+    console.log(password2);
+
+    if (
+      name !== "" &&
+      name !== undefined &&
+      email !== "" &&
+      email !== undefined &&
+      emailValidation(email) &&
+      password1 !== "" &&
+      password1 !== undefined &&
+      password2 !== "" &&
+      password2 !== undefined &&
+      password1 === password2
+    ) {
+      instance
+        .post("signup", {
+          name: name,
+          email: email,
+          password: password2,
+        })
+        .then(function (response) {
+          if (response.status === 200) {
+            window.sessionStorage.setItem("token", response.data.token);
+            history.replace("/");
+          }
+        })
+        .catch((error) => {
+          const output = `<small>${error}</small>`;
+          empty__input.innerHTML = output;
+        });
+    } else {
+      /*const output = `<small>Please enter valid details.</small>`;
+      empty__input.innerHTML = output;*/
+       swal("Alert", "Please enter valid details. ", "error");
+    }
+  };
+   
   return (
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form action="#" className="sign-in-form">
-            <h2 className="title">Sign in</h2>
-            <div className="input-field">
-              <EmailIcon className="icon"/>
-              <input
-                  type="text"
-                  placeholder="Email"
-                  onChange={(e) => handleInput1(e)}
-                  required
-              />
-            </div>
+    <div className="Signin">
+      <div className="container" id="animate">
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="#" className="sign-in-form">
+              <h2 className="title">Sign in</h2>
+
+             <div className="input-field">
+           <EmailIcon className="icon" />
+            <input
+              type="text"
+              placeholder="Email"
+              onClick={(e) => handleInput1(e)}
+            />
+          </div>
+
+          <div className="input-field">
+            <LockIcon className="icon" />
+            <input
+              type="password"
+              placeholder="Password"
+              onClick={(e) => handleInput2(e)} 
+            />
+          </div>
+
+              <input type="submit" value="Sign in" className="btn solid" onClick={(e) => signIn(e)}/>
+
+            </form>
+            {/* End of Sign in  */}
+
+            {/* Start of Signup */}
+            <form action="#" className="sign-up-form">
+              <h2 className="title">Sign up</h2>
 
             <div className="input-field">
-              <LockIcon className="icon"/>
-              <input
-                  type="password"
-                  placeholder="Password"
-                  onChange={(e) => handleInput2(e)}
-                  required
-              />
-            </div>
-            <div className="empty__input"/>
+           <PersonIcon className="icon" />
+            <input
+              type="text"
+              placeholder="Username"
+              onClick={(e) => handleInput1(e)}
+            />
+          </div>
 
-            <button type="submit" className="dark_btn" onClick={(e) => signIn(e)}>
-              Sign In
-            </button>
-          </form>
+
+             <div className="input-field">
+           <EmailIcon className="icon" />
+            <input
+              type="text"
+              placeholder="Email"
+              onClick={(e) => handleInput2(e)}
+            />
+          </div>
+             
+               <div className="input-field">
+           <LockIcon className="icon" />
+            <input
+              type="password"
+              placeholder="Password"
+              onClick={(e) => handleInput3(e)}
+            />
+          </div>
+             
+
+                 <div className="input-field">
+           <LockIcon className="icon" />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              onClick={(e) => handleInput4(e)}
+            />
+          </div>
+             
+
+
+              <input type="submit" className="btn" value="Sign up" onClick={(e) => signUp(e)}/>
+
+            </form>
+            {/* End of signup */}
+          </div>
+        </div>
+
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>New here ?</h3>
+              <p>
+                Create a account with us and take benifits of our system.</p>
+              <button
+                className="btn transparent"
+                id="sign-up-btn"
+                onClick={(e) => signUpAnimate(e)}
+              >
+                Sign up
+              </button>
+            </div>
+          <img src={boyVector} alt="Boy" className="image" />
+          </div>
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>One of us ?</h3>
+              <p>Then what you are doing here . . . . . .</p>
+              <button
+                className="btn transparent"
+                id="sign-in-btn"
+                onClick={(e) => {
+                  signInAnimate(e);
+                }}
+              >
+                Sign in
+              </button>
+            </div>
+             <img src={womenVector} alt="women" className="image" /> 
+          </div>
         </div>
       </div>
+    </div>
   );
-};
+}
+}
 export default Signin;
