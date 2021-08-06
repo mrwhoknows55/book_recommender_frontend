13a2c9332fa762fb0447c804d7040a261562d4be
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2021-08-06 12:26:51 +0530

    base url changed

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: `http://localhost:8000/api/`,
+  baseURL: `http://books-api.mrwhoknows.com/api/`,
 });
 
 export default instance;
