87007c1f177451faeb0ab81770504fae8399985e
Avadhut Tanugade <30384908+mrwhoknows55@users.noreply.github.com>
2021-08-06 23:04:12 +0530

    base url changed to https

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: `http://books-api.mrwhoknows.com/api/`,
+  baseURL: `https://books-api.mrwhoknows.com/api/`,
 });
 
 export default instance;
